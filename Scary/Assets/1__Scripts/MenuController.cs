5b1ae1854b5818ec8df45b6937dfa59520d0be24
Lerbo <qaz431220@gmail.com>
2023-06-08 00:34:01 +0800

    新增 Begin 場景按鈕功能
    Begin 頁面按鈕功能如下
    1. 開始遊戲
    2. 遊戲設定
    3. 遊戲團隊
    4. 離開遊戲
    
    2. 調整場景順序(Build index)

--- Scary/Assets/1__Scripts/MenuController.cs
+++ Scary/Assets/1__Scripts/MenuController.cs
@@ -6,30 +6,46 @@ using UnityEngine.UI;
 public class MenuController : MonoBehaviour
 {
     public bool bLoadSceneAsync;
-    public int iNextSceneID;
+    [SerializeField] int iNextSceneID;
 
-    [SerializeField] Button Btn_StartGame;
+    [SerializeField] Button Btn_EnterGame;
+    [SerializeField] Button Btn_GameSetting;
+    [SerializeField] Button Btn_Team;
+    [SerializeField] Button Btn_TeamReturn;
+    [SerializeField] Button Btn_EndGame;
 
     AsyncOperation async = null;
     public GameObject TineLineAniObj;
-    public Animation aniSubtitle_1;
-    public Animation aniSubtitle_2;
 
     void Start()
     {
-        
-        if (Btn_StartGame == null )
-            Btn_StartGame = GameObject.Find("Canvas/EnterGame").GetComponent<Button>();
+        if (Btn_EnterGame == null)
+            Btn_EnterGame = GameObject.Find("Canvas/EnterGame").GetComponent<Button>();
 
-        if (bLoadSceneAsync)
-        {
-            StartCoroutine(nameof(LoadSceneAsync));
-            Btn_StartGame.onClick.AddListener(() => AsyncActivate());
-        }
-        else
-        {
-            Btn_StartGame.onClick.AddListener(() => LoadScene(iNextSceneID));
-        }
+        if (Btn_GameSetting == null)
+            Btn_GameSetting = GameObject.Find("Canvas/Setting/SettingBtn").GetComponent<Button>();
+
+        if (Btn_Team == null)
+            Btn_Team = GameObject.Find("Canvas/Team/TeamBtn").GetComponent<Button>();
+
+        if (Btn_EndGame == null)
+            Btn_EndGame = GameObject.Find("Canvas/EndGame").GetComponent<Button>();
+
+        Btn_EnterGame.onClick.AddListener(() => LoadScene(iNextSceneID));
+        Btn_GameSetting.onClick.AddListener(() => ShowGameSetting());
+        Btn_Team.onClick.AddListener(() => ShowTeam());
+        Btn_TeamReturn.onClick.AddListener(TeamReturn);
+        Btn_EndGame.onClick.AddListener(() => EndGame());
+
+        //if (bLoadSceneAsync)
+        //{
+        //    StartCoroutine(nameof(LoadSceneAsync));
+        //    Btn_StartGame.onClick.AddListener(() => AsyncActivate());
+        //}
+        //else
+        //{
+        //    Btn_StartGame.onClick.AddListener(() => LoadScene(iNextSceneID));
+        //}
 
     }
 
@@ -38,19 +54,63 @@ public class MenuController : MonoBehaviour
         SceneManager.LoadScene(r_iSceneIndex);
     }
 
-    IEnumerator LoadSceneAsync()
+    void ShowGameSetting()
     {
-        async = SceneManager.LoadSceneAsync(iNextSceneID);
-        async.allowSceneActivation = false;
-
-        while (async.progress < 0.9f)
-        {
-            yield return new WaitForEndOfFrame();
-        }
+        HideAllBtn();
+        GameObject SettingView = Btn_GameSetting.gameObject.transform.parent.GetChild(1).gameObject;
+        SettingView.SetActive(true);
     }
 
-    void AsyncActivate()
+    void ShowTeam()
     {
-        async.allowSceneActivation = true;
+        HideAllBtn();
+        GameObject TeamView = Btn_Team.gameObject.transform.parent.GetChild(1).gameObject;
+        TeamView.SetActive(true);
+        Btn_TeamReturn = GameObject.Find("Canvas/Team/View/Return").GetComponent<Button>();
     }
+
+    void TeamReturn()
+    {
+        ShowAllBtn();
+        Btn_TeamReturn = null;
+        GameObject TeamView = Btn_Team.gameObject.transform.parent.GetChild(1).gameObject;
+        TeamView.SetActive(false);
+    }
+
+    void EndGame()
+    {
+        Application.Quit();
+    }
+
+    void ShowAllBtn()
+    {
+        Btn_EnterGame.gameObject.SetActive(true);
+        Btn_GameSetting.gameObject.SetActive(true);
+        Btn_Team.gameObject.SetActive(true);
+        Btn_EndGame.gameObject.SetActive(true);
+    }
+
+    void HideAllBtn()
+    {
+        Btn_EnterGame.gameObject.SetActive(false);
+        Btn_GameSetting.gameObject.SetActive(false);
+        Btn_Team.gameObject.SetActive(false);
+        Btn_EndGame.gameObject.SetActive(false);
+    }
+
+    //IEnumerator LoadSceneAsync()
+    //{
+    //    async = SceneManager.LoadSceneAsync(iNextSceneID);
+    //    async.allowSceneActivation = false;
+
+    //    while (async.progress < 0.9f)
+    //    {
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //}
+
+    //void AsyncActivate()
+    //{
+    //    async.allowSceneActivation = true;
+    //}
 }
